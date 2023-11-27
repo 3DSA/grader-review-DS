fe83659edb636126ab46b4d1535c33aae0e944f1
3DSA <146782672+3DSA@users.noreply.github.com>
2023-11-27 14:57:12 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -1,3 +1,4 @@
+set -e
 CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 
 rm -rf student-submission
@@ -6,9 +7,16 @@ rm -rf grading-area
 mkdir grading-area
 
 git clone $1 student-submission
+files='find student-submission/ListExamples.java'
+  if [[ -f $files ]] && [[ $files == *ListExamples* ]]
+then
+    echo "Everything is there"
+  else
+    echo "Not there"
+  fi
 echo 'Finished cloning'
 
-
+cp ~/list-examples-grader/student
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
 
